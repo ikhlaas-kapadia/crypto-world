244af81d0d80d64583c3efeecb7e82a69b595e25
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2021-11-04 16:51:25 +0000

    Incorporate pagination with data from api

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,3 +1,4 @@
+import millify from 'millify';
 import React, { useState, useEffect } from 'react';
 import { Container, Col, Card, Row } from 'react-bootstrap';
 
@@ -8,18 +9,18 @@ const CryptoList = (props) => {
     return (
             <Container className="crypto-list-container">
                 <Row>
-                    
-                        {paginatedData && paginatedData.map((item, index) => (
-                            <Col sm={6} md={6} lg={4} xl={3} className="mb-4" key={item.rank}>
+                        {paginatedData && paginatedData.map((currency, index) => (
+                            <Col sm={6} md={6} lg={4} xl={3} className="mb-4" key={currency.rank}>
                                 <Card className="crypto-card">
                                     <Card.Header className="text-center d-flex justify-content-between">
-                                        <span>{item.rank}. {item.data}</span> <span>logo</span>
+                                        <span className="align-self-center">{currency.rank} - {currency.name}</span> 
+                                        <div><img className="coin-icon" src={currency.iconUrl} alt={currency.name} logo/></div>
                                     </Card.Header>
                                     <Card.Body  >
                                         <Card.Text className="text-left">
-                                        <p>Price: <span className="list-value">46k</span></p>
-                                        <p>Market Cap: <span className="list-value">3b</span></p>
-                                        <p>Daily change: <span className="list-value">25%</span></p>
+                                        <p>Price:  <span className="list-value">${millify(currency.price)}</span></p>
+                                        <p>Market Cap: <span className="list-value">{millify(currency.marketCap)}</span></p>
+                                        <p>Daily change: <span className="list-value">{currency.change < 0 ? currency.change : '+' + currency.change} %</span></p>
                                         </Card.Text>
                                     </Card.Body>
                                 </Card>
@@ -33,31 +34,33 @@ const CryptoList = (props) => {
 
 const Pagination = (props) => {
     const {data} = props;
+
     const [pages, setPages] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(props.itemsPerPage);
     const [firstPage, setFirstPage] = useState(null);
     const [lastPage, setLastPage] = useState(null);
     const [endIndex, setEndIndex] = useState(null);
     const [startIndex, setStartIndex] = useState(null);
-    const [paginatedData, setPaginatedData] = useState([]);
+    const [paginatedData, setPaginatedData] = useState(null);
     
 
 
     useEffect(()=>{
-        if(props.data) {
-            setPages(Math.ceil(props.data.length / props.itemsPerPage));
-            setItemsPerPage(props.itemsPerPage);
-            setFirstPage(props.data.length - (props.data.length - 1));
-            setLastPage(Math.ceil(props.data.length / props.itemsPerPage));
-            let endIndex = (currentPage * itemsPerPage) - 1;
-            let startIndex = (endIndex - itemsPerPage) + 1;
+        if(data.coins.length > 0) {
+            setPages(Math.ceil(data.coins.length / props.itemsPerPage));
+            setFirstPage(data.coins.length - (data.coins.length - 1));
+            setLastPage(Math.ceil(data.coins.length / props.itemsPerPage));
+            let endIndex = (currentPage * props.itemsPerPage) - 1;
+            let startIndex = (endIndex - props.itemsPerPage) + 1;
             let paginatedData = [];
             for (let i = startIndex; i <= endIndex; i++) {
-                const element = {'rank': i + 1, 'data': props.data[i]};
-                paginatedData.push(element);
+                const element = data.coins[i];
+                if(element) {
+                    paginatedData.push(element);
+                }
                 
             }
+          
             setEndIndex(endIndex);
             setStartIndex(startIndex);
             setPaginatedData(paginatedData);
@@ -68,18 +71,17 @@ const Pagination = (props) => {
     const handlePageChange = (e) => {
 
             console.log('clicked');
-            console.dir(e);
-            setCurrentPage(e.target.value);
+             setCurrentPage(Number(e.target.childNodes[0].data));
         
     }
 
     const handlePrevNextChange = (direction) => {
-           if(direction === 'increment') {
+           if(direction === 'increment' && currentPage < lastPage) {
              console.log('incremented');
             setCurrentPage((currentPage) => {
                 return currentPage + 1;
             })
-        } else if(direction === 'decrement'){
+        } else if(direction === 'decrement' && currentPage > firstPage){
              console.log('decremented');
       
              setCurrentPage((currentPage) => {
@@ -92,22 +94,21 @@ const Pagination = (props) => {
     return (
         <>
         <CryptoList paginatedData={paginatedData}/>
-        {pages > 1 &&  
-        <div className="container">
+        {pages > 0 && <div className="container">
             <div className="row pagination-container justify-content-center">
             <nav aria-label="Page navigation example">
                 <ul className="pagination">
-                    <li className="page-item" onclick={(e)=>{ handlePageChange(e)}}> <a className="page-link active" href="#">{firstPage}</a> </li>
-                    <li className="page-item" ><a className="page-link" href="#" aria-label="Previous" onClick={(e)=>{handlePrevNextChange('decrement')}} ><span aria-hidden="true">&laquo;</span></a></li>
+                    <li className="page-item" onClick={(e)=>{ handlePageChange(e)}}> <a className="page-link active" href="#">{firstPage}</a> </li>
+                    <li className="page-item" ><a className="page-link" href="#" aria-label="Previous" onClick={(e)=>{handlePrevNextChange('decrement')}}><span aria-hidden="true">&laquo;</span></a></li>
                     <li className="page-item"> <a className="page-link active" href="#">{currentPage}</a> </li>
                     <li className="page-item" ><a className="page-link" href="#" aria-label="Next" onClick={(e)=>{handlePrevNextChange('increment')}}><span aria-hidden="true">&raquo;</span></a></li>
-                    <li className="page-item" onclick={(e)=>{ handlePageChange()}}> <a className="page-link active" href="#" >{lastPage}</a> </li>
+                    <li className="page-item" onClick={(e)=>{ handlePageChange(e)}}> <a className="page-link active" href="#">{lastPage}</a> </li>
 
                 </ul>
             </nav>
             </div>
         </div>}
-       
+        
         </>
     );
 };
