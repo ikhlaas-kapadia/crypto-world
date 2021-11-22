d9f92bb1544ad392ffde3a24ab0b049d159e13da
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2021-11-22 20:53:40 +0000

    Add query paramters to api fetch function

--- src/api/api.js
+++ src/api/api.js
@@ -4,14 +4,16 @@ import axios from "axios";
 
 //Fetch Summary stats from proxy api 
 async function fetchData (page, params = '') {
-    let url = `http://localhost:4999/cryptoworld/${page}?${params}`;
+    let url = `http://localhost:4999/cryptoworld/${page}/${params}`;
+    // console.log(url);
+ 
     
     try {
         let response = await axios.get(url);
-        let {data}  = await response.data;
+        let data  = await response.data;
         
     
-        // console.log(data);
+        console.log(data);
         return data;
 
     } catch(error) {
