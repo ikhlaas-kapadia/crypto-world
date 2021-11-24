2b65702e4fbe0123c974e327094309e23366483e
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2021-11-24 21:33:50 +0000

    Modify price chart

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -75,15 +75,8 @@ const PriceChart = (props) => {
     return (
         <>
             <div className='header'>
-            <h1 className='title'>Line Chart</h1>
-            <div className='links'>
-                <a
-                className='btn btn-gh'
-                href='https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/Line.js'
-                >
-                Github Source
-                </a>
-            </div>
+            <h1 className='title text-center'>Price chart</h1>
+         
             </div>
             <Line data={data} options={options} />
         </>)
